4ee02ad02abebfbd7c3700515408ff4e5987fa35
seaniiio <cnc4750@naver.com>
2024-03-27 20:30:27 +0900

    경로 수정

--- 실습3.py
+++
@@ -1,5 +0,0 @@
-siwon = ['백엔드', '김시원', 'isfp', 23]
-
-s = '안녕하세요 저는 멋쟁이 사자처럼 %s파트이고\n이름은 %s입니다\n제 mbti는 %s이고\n나이는 %d살 입니다.' 
-
-print(s % (siwon[0], siwon[1], siwon[2], siwon[3]))
--- 실습5.py
+++
@@ -1,9 +0,0 @@
-grade = int(input("성적을 입력하세요."))
-if grade >= 90:
-    print('A')
-elif grade >= 80:
-    print('B')
-elif grade >= 70:
-    print('C')
-else:
-    print('F')
