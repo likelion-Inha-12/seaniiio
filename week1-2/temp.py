d6b979f2d05ae8cb24900d6325ed3283b5a802c5
시원 <121426422+seaniiio@users.noreply.github.com>
2024-03-27 20:48:38 +0900

    Delete week1-2/temp.py

--- week1-2/temp.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-fake = Faker('ko-KR')
-fake.name()
-fake.address()
-print(fake.name())
-print(fake.address())
-
-test_data = [(fake.name(), fake.address()) for i in range(30)]
-print(test_data)
