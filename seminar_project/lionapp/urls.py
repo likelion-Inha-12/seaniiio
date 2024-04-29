28908ca50c0905c8ca83ee9b5d645bcc3e0f0bf4
seaniiio <cnc4750@naver.com>
2024-04-29 19:49:35 +0900

    week 4: 심화 과제

--- seminar_project/lionapp/urls.py
+++ seminar_project/lionapp/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('create/', views.create_post), # 이 urls.py에는 ‘lion/create/’ 로 요청이 들어올 때, 우리가 만든 views.py 의 create_post 함수를 실행시키는 로직을 담고 있습니다.
     path('<int:pk>/', views.get_post), # int라는 타입의 pk 인자를 받겠다. get_post함수에 pk라고 정의했기 때문에 꼭 pk라고 적어줘야 함. ex) `lion/1`
     path('delete/<int:pk>/', views.delete_post),
-    path('comments/<int:post_id>', views.get_comment)
+    path('comments/<int:post_id>/', views.get_comment),
+    path('like/<int:post_id>/<int:user_id>/', views.post_like),
+    path('likes/<int:post_id>/', views.get_like_count),
+    path('posts/', views.get_post_sorted)
 ]
--- seminar_project/lionapp/views.py
+++ seminar_project/lionapp/views.py
@@ -50,3 +50,53 @@ def get_comment(request, post_id):
         post = get_object_or_404(Post, pk=post_id)
         comment_list = post.comments.all() # Post에 comments필드가 있는 것은 아니지만, models.py에 related_name으로 정의해주었음. // comments => comment_set으로 변경해도 같은 기능을 하지만, relate_name을 설정해놓으면 작동 안함
         return HttpResponse(comment_list, status=200)
+    
+# user_id와 post_id를 request로 받고 좋아요를 누르는 api
+def post_like(request, post_id, user_id):
+    if request.method == 'POST':
+
+        # 이미 사용자가 해당 게시물에 좋아요를 누른 경우
+        if UserPost.objects.filter(user_id=user_id, post_id=post_id).exists():
+            return HttpResponse("이미 좋아요를 눌렀습니다.", status=409)
+        
+        user = Member.objects.get(pk = user_id)
+        post = Post.objects.get(pk = post_id)
+
+        userPost = UserPost(
+            user_id = user,
+            post_id = post
+        )
+        userPost.save()
+
+        return HttpResponse(status=204)
+
+# post_id를 request로 받아 좋아요 개수를 반환하는 api
+def get_like_count(request, post_id):
+    if request.method == 'GET':
+
+        # 해당 post가 존재하지 않는 경우 404
+        post = get_object_or_404(Post, pk=post_id)
+
+        like_count = UserPost.objects.filter(post_id = post_id).count()
+
+        return JsonResponse({"like_count":like_count})
+
+# 댓글이 많이 달린 순으로 post를 정렬하여 리스트로 반환하는 api 
+def get_post_sorted(request):
+    if request.method == 'GET':
+
+        post_comment_counts = [] # 모든 post에 대해 (post, comment 개수) 형태로 저장
+
+        posts = Post.objects.all() # post들 불러오기
+
+        for post in posts:
+            # 역참조를 이용해서 댓글 개수 읽어오기
+            comment_count = post.comments.all().count() # related_name = 'comments'
+            post_comment_counts.append((post, comment_count))
+        
+        # sort
+        post_comment_counts.sort(key = lambda x:-x[1]) # comment_count가 높은 순서대로 sort
+        sorted_post_list = [p[0] for p in post_comment_counts] # post만 빼와서 return할 리스트 생성
+
+        return HttpResponse(sorted_post_list, status=200)
+# sorted_posts = Post.objects.annotate(comment_count=Count('comments')).order_by('-comment_count') 
