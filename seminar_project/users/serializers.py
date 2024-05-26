32f6f0438aab160a54b3df82ef8251270ebf97dd
seaniiio <cnc4750@naver.com>
2024-05-27 01:09:25 +0900

    week 8 기본 과제

--- seminar_project/users/serializers.py
+++ seminar_project/users/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'email', 'password', 'name')
+        fields = ('id', 'email', 'password', 'name', 'generation', 'gender')
 
 # 패스워드가 필요없는 다른 테이블에서 사용할 용도
 class UserInfoSerializer(serializers.ModelSerializer):
--- seminar_project/users/views.py
+++ seminar_project/users/views.py
@@ -8,18 +8,31 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from users.models import User
 from users.serializers import UserSerializer
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def signup(request):
+    id = request.data.get('id')
+
+    if User.objects.filter(id=id).exists(): # id 중복 여부 체크
+        return Response({'error': '이미 사용중인 ID입니다.'}, status=status.HTTP_409_CONFLICT)
+
     email = request.data.get('email')
     password = request.data.get('password')
     name = request.data.get('name')
+    generation = request.data.get('generation')
+    gender = request.data.get('gender')
+
+    if gender != 'M' and gender != 'F':
+        return Response({'error': '성별은 M 혹은 F만 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
 
     serializer = UserSerializer(data=request.data)
     serializer.email = email
     serializer.name = name
+    serializer.generation = generation
+    serializer.gender = gender
 
     if serializer.is_valid(raise_exception=True):
         user = serializer.save()
@@ -32,10 +45,10 @@ def signup(request):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login(request):
-    email = request.data.get('email')
+    id = request.data.get('id')
     password = request.data.get('password')
 
-    user = authenticate(email=email, password=password)
+    user = authenticate(id=id, password=password)
     if user is None:
         return Response({'message': '아이디 또는 비밀번호가 일치하지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
 
